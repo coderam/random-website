de9c4ec2dd6a091e54d9dd4942106cec39c3285e
coderam <959477+coderam@users.noreply.github.com>
2018-04-24 19:57:44 -0500

    Update random.supported.sites.js

--- random.supported.sites.js
+++ random.supported.sites.js
@@ -3,6 +3,12 @@ addToRandomSupportedSites(
 		'https://en.wikipedia.org/wiki/Special:Random',
 		'http://www.howstuffworks.com/random-article',
 		'http://www.wikihow.com/Special:Randomizer',
-		'http://www.fromdev.com/random',
+		'http://www.pbase.com/photos/random.html',
+		'https://bighugelabs.com/random.php',
+		'https://www.poetrysoup.com/poems/random_member_poems.aspx',
+		'https://www.qwant.com/?q=mr%20math%20middle%20school&client=opensearch&freshness=day'
+	
+	
+		
 ]
 );
