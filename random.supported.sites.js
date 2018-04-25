bb8d7d2f613747b08b86df72b7ec30aa2f91e05b
coderam <959477+coderam@users.noreply.github.com>
2018-04-24 21:10:51 -0500

    Update random.supported.sites.js

--- random.supported.sites.js
+++ random.supported.sites.js
@@ -6,7 +6,7 @@ addToRandomSupportedSites(
 		'https://www.qwant.com/?q=mr%20math%20middle%20school&client=opensearch&freshness=day',
 		'https://picsum.photos/874/874/?random',
 		'https://picsum.photos/864/874/?random',
-		'https://picsum.photos/544/544/?random'
+		'https://picsum.photos/544/544/?random',
 		'https://picsum.photos/634/634/?random'
 		
 ]
