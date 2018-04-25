88b34888f3b8743232a25659fbc33dd2dff76270
coderam <959477+coderam@users.noreply.github.com>
2018-04-24 21:10:34 -0500

    Update random.supported.sites.js

--- random.supported.sites.js
+++ random.supported.sites.js
@@ -2,11 +2,12 @@ addToRandomSupportedSites(
 [
 		'https://en.wikipedia.org/wiki/Special:Random',
 		'http://www.pbase.com/photos/random.html',
-		'https://bighugelabs.com/random.php',
 		'https://www.poetrysoup.com/poems/random_member_poems.aspx',
-		'https://www.qwant.com/?q=mr%20math%20middle%20school&client=opensearch&freshness=day'
-	
-	
+		'https://www.qwant.com/?q=mr%20math%20middle%20school&client=opensearch&freshness=day',
+		'https://picsum.photos/874/874/?random',
+		'https://picsum.photos/864/874/?random',
+		'https://picsum.photos/544/544/?random'
+		'https://picsum.photos/634/634/?random'
 		
 ]
 );
