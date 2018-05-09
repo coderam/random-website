aaf4a823cd1fe51344d07b441cd425edb29ac31b
rasberrypi <pi@rasberrypi.com>
2018-05-09 00:40:19 +0000

    new

---
+++ browser.reboot.sh
@@ -0,0 +1,13 @@
+sleep 6
+
+cd /home/pi/random-website && git pull &
+cd /home/pi/random-website && epiphany-browser index.html &
+
+sleep 6969
+
+rm /home/pi/.cache -rf &
+
+sleep 6
+
+sleep 7272 && reboot &
+
