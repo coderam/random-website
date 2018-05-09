557bed877ae21f60b2386f49e8286c44ce43da09
Your Name <user@email.com>
2018-05-09 11:36:42 -0500

    ran

--- random.supported.sites.js
+++ random.supported.sites.js
@@ -1,10 +1,6 @@
 addToRandomSupportedSites(
 [
-		'http://www.pbase.com/photos/random.html',
-		'https://picsum.photos/874/874/?random',
-		'https://picsum.photos/864/874/?random',
-		'https://picsum.photos/544/544/?random',
-		'https://picsum.photos/634/634/?random'
+		'http://www.pbase.com/photos/random.html'
 		
 ]
 );
